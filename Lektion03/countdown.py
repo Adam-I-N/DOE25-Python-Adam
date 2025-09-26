183097d4a8af88cccf769f5ac0a2094eba6febea
Adam-I-N <adam.i.n@outlook.com>
2025-09-26 10:59:43 +0200

    removed count uo loop

--- Lektion03/countdown.py
+++ Lektion03/countdown.py
@@ -1,9 +1,5 @@
 import time
 
-# [0, 1, 2]. I koden är 3 alltså hur många siffor som räknas. Den börjar på noll och slutar då på 2.
-for j in range(3):
-    print(j)
-
 #[5, 4, 3, 2, 1]. 
 for i in range(5, 0, -1):
     print(i)
