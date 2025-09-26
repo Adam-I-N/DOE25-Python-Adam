675f6271d161657a7d7aca946f10cf209c2faa66
Adam-I-N <adam.i.n@outlook.com>
2025-09-26 11:03:30 +0200

    added count up feature

--- Lektion03/countdown.py
+++ Lektion03/countdown.py
@@ -1,5 +1,9 @@
 import time
 
+# [0, 1, 2]. I koden är 3 alltså hur många siffor som räknas. Den börjar på noll och slutar då på 2.
+for j in range(3):
+    print(j)
+
 #[5, 4, 3, 2, 1]. 
 for i in range(5, 0, -1):
     print(i)
